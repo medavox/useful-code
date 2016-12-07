013b274bcfe80ff546bab134cb1c0a4c701750f9
Adam Howard <medavox@users.noreply.github.com>
2016-12-07 12:16:00 +0000

    added comments to alertNotGrowing.sh

--- alertNotGrowing.sh
+++ alertNotGrowing.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+#alerts the user if the file in $1 hasn't changed for a while
 while [ 1 ] ; do
 	last_size=$size
-	size=$(du /media/noob/aimDebDrive.hdd)
+	size=$(du $1)
 	echo $size
 	if [ "$last_size" = "$size" ] ; then
+		#add an appropriate alert here
 		mplayer -novideo /home/adam/Videos/omd.mp4
 		break
 	fi
