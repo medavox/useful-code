e2c6cc17075a0a18aed0af49ff5a5d5cb41e7fb1
Adam Howard <medavox@users.noreply.github.com>
2018-11-24 14:37:00 +0000

    Create oldSchoolForLoop.kt

---
+++ oldSchoolForLoop.kt
@@ -0,0 +1,15 @@
+fun <T> fur(initializer: () -> T,
+            loopCheck:(T) -> Boolean,
+            update:(T) -> T,
+            loopBody:(T) -> Unit) {
+    var index:T = initializer()
+    while(loopCheck(index)) {
+        loopBody(index)
+        index = update(index)
+    }
+}
+
+//usage:
+fur({0}, {it < array.size}, {it+1}) {//rename index variable here : `index ->`
+    //do stuff with `it`
+}
